83b73e5435a39b935a58640dfe9c1fb78b8f3dcd
Gabriel Plassard <gabriel.plassard@gmail.com>
2019-11-05 21:25:26 +0100

    start.spring.io

---
+++ src/main/java/fr/gplassard/jpaconverter/jpaconverter/JpaconverterApplication.java
@@ -0,0 +1,13 @@
+package fr.gplassard.jpaconverter.jpaconverter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaconverterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaconverterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/gplassard/jpaconverter/jpaconverter/JpaconverterApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.gplassard.jpaconverter.jpaconverter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaconverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
